bf554a68aabdbb1c6b252609a9d167d6ba6f41a7
Devesh <deveshshukla224@gmail.com>
2022-04-20 15:34:07 +0530

    blog detaila and input form

--- src/Create.js
+++ src/Create.js
@@ -1,8 +1,36 @@
-import React from "react";
+import React, {useState} from "react";
 const Create = ()=>{
+    const [title, setTitle] = useState("");
+    const [body, setBody] = useState("");
+    const [author , setAuthor] = useState("Mario");
     return(
      <div className="create">
-         <h1>Add a New Blog</h1>
+         <h2>Add a New Blog</h2>
+         <label> Blog Title : </label>
+         <input
+         type="text"
+         required
+         value={title}
+         onChange={(e)=>{ setTitle(e.target.value)}}
+         />
+         <label>Blog Body :</label>
+         <textarea
+             required
+             value={body}
+             onChange={(e)=>setBody(e.target.value)}
+         ></textarea>
+         <label> Author : </label>
+         <select
+         value={author}
+         onChange={(e)=>{
+             setAuthor(e.target.value)
+         }}
+         >
+             <option value="Mario">Mario</option>
+             <option value="Yoshi">Yoshi</option>
+         </select>
+
+         <button>Add Blog</button>
      </div>
     );
 }
